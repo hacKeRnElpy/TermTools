750dc00ab20852e6261eec4672fad6ba97e4614c
hacKeRnElpy <realaipaii@gmail.com>
2020-07-16 11:19:38 +0300

    v-0.9

---
+++ termtools.sh
@@ -0,0 +1,50 @@
+clear
+apt install git
+clear
+while :
+do
+echo -e "[1]DDoS"
+echo -e "[2]Vim"
+echo -e "[3]Metasploit"
+echo -e "[4]Recon-ng"
+echo -e "[5]Nmap"
+echo -e "[0]Exit"
+echo -e ""
+read -p '  [TermTools] >> ' userinput1
+clear
+if [ "${userinput1:-}" = "1" ]
+then
+  cd
+  git clone https://github.com/Andrey-oss/ddoser.git
+  cd TermTools
+  clear
+fi
+if [ "${userinput1:-}" = "2" ]
+then
+  apt install vim
+  clear
+fi
+if [ "${userinput1:-}" = "3" ]
+then
+  apt install metasploit
+  clear
+fi
+if [ "${userinput1:-}" = "4" ]
+then
+  cd
+  git clone https://github.com/lanmaster53/recon-ng
+  cd TermTools
+  clear
+fi
+if [ "${userinput1:-}" = "5" ]
+then
+  cd
+  git clone https://github.com/nmap/nmap
+  cd TermTools
+  clear
+fi
+if [ "${userinput1:-}" = "0" ]
+then
+  break
+fi
+done
